36417b2f70b85949c313caba60abfac7a5cffe74
Sumit Joshi <sumit.joshi0616@gmail.com>
2025-01-13 03:30:31 +0530

    done

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "../config.env" });
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,14 +13,20 @@ const db = process.env.DATABASE_URL.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-mongoose.connect(db, {}).then(() => {
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  
   console.log("Connected to MongoDB");
 });
-app.get("/", (req, res) => {
+
+
+app.get("/", async (req, res) => {
   res.status(200).json({
-    data: "Vaibhav and Sumit \n Make this API Functional",
   });
 });
+
 app.listen(process.env.PORT, () => {
   console.log("Server connected ");
 });
